43c41eba754e391b551a154a72441c5566322af4
asma496 <rude.fati@gmail.com>
2020-10-28 12:58:55 +0500

    my file

---
+++ java/index.js
@@ -0,0 +1,77 @@
+// alert
+//     ("Asma \n Ashraf")
+
+
+//  var birthYear;
+//     birthYear = ["1996", "asma"];
+//     alert(` type of variable birthYear is = ` + typeof birthYear  )
+
+
+
+//     var heading = "Rules for naming JS variables"
+//     document.write ("“Rules for naming JS variables”");
+
+
+
+// var age = prompt( " enter Your Age ")
+// var ageInDays = age * 365;
+// console.log ( "My age in days is = "  + ageInDays)
+
+
+
+//  var num = 5+2
+//  var anothernum = ++num
+//  console.log (num);
+
+
+
+//  var num = 5+2
+//  var anothernum = --num
+//  console.log (num);
+
+
+
+
+
+
+
+
+// var num = (2*2)
+// console.log(num)
+
+// var num = prompt("enter your number")
+// if (num > 0){
+//     alert( )
+// }
+
+
+
+// var number;
+// number = -12;
+// alert(` type of variable number is = ` + typeof number  )
+
+
+
+
+var city = prompt(" where do you live ?");
+if
+    (city ==  karachi) {
+        alert ("Welcome to city of lights");
+    }
+    else if
+    (city == Lahore) {
+        alert ("welcome to historical city");
+    }
+  else if 
+     (city == Quetta) {
+         alert("welcome to cold city");
+     }
+
+
+
+
+
+//      var a = 2
+//      var b = 1
+//      var num =  --a - --b + ++b + b--
+//      console.log (num)
