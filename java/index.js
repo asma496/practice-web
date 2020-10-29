1ab74b8821c8b74d237d9ea4768cb409528ebe13
asma496 <rude.fati@gmail.com>
2020-10-29 13:03:18 +0500

    my file

--- java/index.js
+++ java/index.js
@@ -78,16 +78,21 @@
 
 //Q no. 9
 
+// var us = prompt("Enter your US amount")
+// var sau = prompt("Enter your saudia amount")
+// var total = us * 163 + sau * 43;
+// alert(total + " "  + "in pak rupees");
+
 
 
 
 //Q no. 10
 
 
-var a = 2
-var b = 1
-var num =  --a; --a - --b; --a - --b + ++b; --a - --b + ++b + b--;
-console.log (num)
+// var a = 2
+// var b = 1
+// var num = --a - --b + ++b + b--;
+// console.log (num)
 
 
 
@@ -124,47 +129,68 @@ console.log (num)
 
 //Q no. 12
 
-<<<<<<< HEAD
-// var city = +prompt(" where do you live ?")
-// if
-// (city ===  "karachi") {
-//         alert ("Welcome to city of lights")
+
+// var first = +prompt("first val" ,4);
+// var sec = +prompt("sec val" ,3);
+// var op = prompt("operater", "+" , );
+
+// if(op == "+"){
+//     alert(first+sec)
+// } else if(op == "/"){
+//     alert(first/sec)
+// } else if(op == "-"){
+//     alert(first-sec)
+// } else if ( op == "*"){
+//     alert(first*sec)
+
+// } else if (op == "%"){
+//     alert(first/sec*100+ "%");
+
+// }else{
+//     alert(invalid)
+// }
+
+
+
+// for( var i = 0; i < 30; i++){
+//     console.log(i,"Asma")
+// }
+
+// var old =["asma","madiha","komal","iqra","hina","fatima"];
+// var newst = ["tuba", "hijab", "sobia", "kanwal", "iqu", "soha"];
+// var fullstu = [];
+// if(old.length === newst.length){
+// for(var i=0; i<old.lenght; i++){
+//    fullstu[i] = old[i]+ " " +newst[i];
+// }
+// console.log(  fullstu);
+// }
+
+
+
+// var old =["asma","madiha","komal","iqra","hina","fatima"];
+// var newst = ["tuba", "hijab", "sobia", "kanwal", "iqu", "soha"];
+// var fullstu = [];
+// if(old.length === newst.length){
+// for(var i=0; i<old.length; i++){
+//    fullstu[i] = old[i]+ " " +newst[i];
+// }
+// console.log(fullstu)
+// }
+
+
+
+
+
+
+// var inp = prompt("type the name")
+// var names =["asma","madiha","aimen","iqra","hina","fatima"];
+// for(var i =0; i<names.length; i++){
+//     if(inp == names[i]){
+//         alert("found")
 //     }
-// else if
-//     (city === "Lahore") {
-//         alert ("welcome to historical city");
-//     }
-// else if 
-//      (city === "Quetta") {
-//          alert("welcome to cold city");
-//      }
+// }
 
 
-var city = prompt(" where do you live ?")
- if
- (city ===  "karachi") {
-         alert ("Welcome to city of lights")
-    }
-=======
-
-var first = +prompt("first val" ,4);
-var sec = +prompt("sec val" ,3);
-var op = prompt("operater", "+" , );
->>>>>>> f9f638a42c499e105462d3d4b526c692e3af10d6
 
 
-if(op == "+"){
-    alert(first+sec)
-} else if(op == "/"){
-    alert(first/sec)
-} else if(op == "-"){
-    alert(first-sec)
-} else if ( op == "*"){
-    alert(first*sec)
-
-} else if (op == "%"){
-    alert(sec + "% of " + first + " is " + percent);
-
-}else{
-    alert(invalid)
-}
