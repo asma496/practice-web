49f89358840620096ac375351b7772d323f7ce5a
asma496 <asmaashraf496@gmail.com>
2020-11-06 13:08:10 +0500

    my file

---
+++ event.js
@@ -0,0 +1,49 @@
+// function num() {
+//     var name = "asma here"
+//     console.log(name)
+// }
+
+
+// function doo() {
+//     document.getElementById("myh2").innerHTML = 'i am beautiful';
+// }
+
+// function change() {
+//     var x = document.getElementById("para")
+//     var txt = x.innerHTML;
+//     x.innerHTML = "my new class"
+
+//     x.className = "gud"
+//     console.log(x.tagName)
+
+
+// }
+
+
+
+
+function form() {
+
+    var country;
+
+    var result = document.getElementById("cityname").value;
+
+    switch (result) {
+
+        case countryname = "america":
+            cityName = "Chicago";
+
+            break;
+
+        case countryname = "pakistan":
+            cityName = "karachi";
+
+            break;
+
+        case countryname = "india":
+            cityName = "bombay";
+
+    }
+    document.getElementByset("countryname").value = countryname;
+
+}
