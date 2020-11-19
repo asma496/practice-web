ea58bb5bd8dee5bd2767b0bcda059186993de916
asma496 <asmaashraf496@gmail.com>
2020-11-19 13:18:55 +0500

    done
    git add .

--- ES6(advanced js)/script.js
+++ ES6(advanced js)/script.js
@@ -161,8 +161,111 @@
 // var  result =sum(45,22,2);
 // console.log(result);
 
-function check(...op){
-    let [a,b,c]=op
-    console.log(a,b,c)
+// function check(...op){
+//     let [a,b,c]=op
+//     console.log(a,b,c)
+// }
+// check(1,2,3)
+
+// var obj ={
+//     name:' Name:Asma,',
+//     Qualification:'Qualification: Graduate,',
+//     age:'age:16,',
+//     status:'status:not well',
+// };
+// let {name,Qualification,age,status}=obj;
+// console.log(`${name}\n${Qualification}\n${age}\n${status}`);
+// function myfunc(){
+//     var a = 'hello asama';
+//     console.log(a);
+// }
+
+// try{
+//     myfunc();
+
+// }
+// catch(error){
+//     console.log(error);
+// }
+
+
+
+// let click1 = ()=>{
+//     setTimeout(display,3000);
+// }
+// let display = ()=>{
+//     alert('asma')
+//     alert('madiha')
+// }
+
+let click1 = ()=>{
+    setInterval(display,1000);
 }
-check(1,2,3)
+
+let display =()=>{
+    var date = new Date();
+    var day = date.getDate();
+    var month = date.getMonth();
+    var year = date. getFullYear();
+    var hours = date.getHours();
+    var minutes= date.getMinutes();
+    var sec = date.getSeconds();
+    
+    document.getElementById('clockdiv').innerHTML =`${day}-  ${month}-${year} ${hours}:${minutes}:${sec}`;   
+       
+    }
+            
+          
+
+
+
+
+
+
+
+
+
+
+
+// function getTimeRemaining(endtime) {
+//     const total = Date.parse(endtime) - Date.parse(new Date());
+//     const seconds = Math.floor((total / 1000) % 60);
+//     const minutes = Math.floor((total / 1000 / 60) % 60);
+//     const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+//     const days = Math.floor(total / (1000 * 60 * 60 * 24));
+    
+//     return {
+//       total,
+//       days,
+//       hours,
+//       minutes,
+//       seconds
+//     };
+//   }
+  
+//   function initializeClock(id, endtime) {
+//     const clock = document.getElementById(id);
+//     const daysSpan = clock.querySelector('.days');
+//     const hoursSpan = clock.querySelector('.hours');
+//     const minutesSpan = clock.querySelector('.minutes');
+//     const secondsSpan = clock.querySelector('.seconds');
+  
+//     function updateClock() {
+//       const t = getTimeRemaining(endtime);
+  
+//       daysSpan.innerHTML = t.days;
+//       hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+//       minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+//       secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+  
+//       if (t.total <= 0) {
+//         clearInterval(timeinterval);
+//       }
+//     }
+  
+//     updateClock();
+//     const timeinterval = setInterval(updateClock, 1000);
+//   }
+  
+//   const deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+//   initializeClock('clockdiv', deadline);
